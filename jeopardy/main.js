9d2af7246d5bb1596fb351c21f8d69fd611e4532
Jonathan Shapiro <shapiroj18@gmail.com>
2022-02-05 12:41:07 -0500

    basic search url is working

--- jeopardy/main.js
+++ jeopardy/main.js
@@ -2,3 +2,11 @@
 
 const rp = require('request-promise');
 const url = 'https://www.j-archive.com/showgame.php?game_id=7259'
+
+rp(url)
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (err) {
+        console.log(err);
+    })
