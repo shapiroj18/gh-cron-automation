de0ec8a43cf6c77f28b942a1e16d68336ab7f46d
Jonathan Shapiro <shapiroj18@gmail.com>
2022-02-05 13:08:39 -0500

    basic parsing with cheerio working

--- jeopardy/main.js
+++ jeopardy/main.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
 const rp = require('request-promise');
+const cheerio = require('cheerio');
+
 const url = 'https://www.j-archive.com/showgame.php?game_id=7259'
 
 rp(url)
     .then(function (response) {
-        console.log(response);
+        const $ = cheerio.load(response)
+        console.log($('#jeopardy_round').html())
     })
     .catch(function (err) {
         console.log(err);
